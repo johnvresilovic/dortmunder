0933c0530520f1bd4739a8a0cbc32ec4d9fe8631
John Vresilovic <johnvresilovic@protonmail.com>
2022-12-13 15:29:53 -0500

    adding edit, delete and update

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ const fs = require('fs')
 const app = express()
 const port = 3000
 const dortmunder = require('./models/dortmunder.js')
+const methodOverride = require('method-override')
 
 //Middleware
 app.use((req, res, next) => {
   console.log("I run for the ice cream truck.  I do not run for the bus.")
-  next();
+  next()
 })
 app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
 //Views
 app.set('views', './views')
@@ -24,7 +26,7 @@ const db = mongoose.connection
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.set('strictQuery', true)
 mongoose.connection.once("open", () => {
-  console.log("connected to mongo");
+  console.log("connected to mongo")
 })
 
 //Root
@@ -48,8 +50,18 @@ app.get("/dortmunder/new", (req, res) => {
   })
 
 //Delete
+app.delete('/dortmunders/:id', (req, res)=>{
+  dortmunder.findByIdAndRemove(req.params.id, (err, data)=>{
+      res.redirect('/dortmunders')//redirect back to fruits index
+  })
+})
 
 //Update
+app.put('/dortmunders/:id', (req, res)=>{
+  dortmunder.findByIdAndUpdate(req.params.id, req.body, (err, updatedDortmunder)=>{
+      res.redirect(`/dortmunders/${req.params.id}`)
+  })
+})
 
 //Create (or Add)
 app.post("/dortmunder", (req, res) => {
@@ -57,8 +69,34 @@ app.post("/dortmunder", (req, res) => {
       res.redirect("/dortmunder")
     })
   })
+  //Seed route
+  app.get('/dortmunders/seed', (req, res)=>{
+    dortmunder.create([
+        {
+            name:"What's the Worst That Could Happen?",
+            year:1996,
+            synopsis:"Dortmunder seeks revenge against the billionaire who robbed him."
+        },
+    ], (err, data)=>{
+        res.redirect('/dortmunders')
+    })
+})
 
 //Edit
+app.get('/dortmunders/:id/edit', (req, res)=>{
+  dortmunder.findById(req.params.id, (err, foundFruit)=>{ //find the fruit
+    if(!err){
+      res.render(
+        'Edit',
+      {
+        dortmunder: foundDortmunder //pass in the found fruit so we can prefill the form
+      }
+    )
+  } else {
+    res.send({ msg: err.message })
+  }
+  })
+})
 
 //Show
 app.get("/dortmunder/:bookId", function (req, res) {
--- views/Index.jsx
+++ views/Index.jsx
@@ -1,24 +1,28 @@
 const React = require("react")
+const DefaultLayout = require('./Default.jsx')
 class Index extends React.Component {
   render() {
     return (
-      <div>
-        <h1>The Dortmunder Series</h1>
+      <DefaultLayout title={'The Dortmunder Series'}>
         <h2>Comic Novels by Donald E. Westlake</h2>
         <ul>
           {this.props.dortmunder.map((dortmunder, i) => {
             return (
               <li>
-                <a href={`/dortmunder/${dortmunder.id}`}>{dortmunder.title}</a>{" "}
-                <br />
+                <a href={`/dortmunder/${dortmunder.id}`}>{dortmunder.title}</a>{" "}<br />
+                <a href={`/dortmunders/${dortmunder._id}/edit`}>Edit</a><br />
+                <form action={`/dortmunders/${dortmunder._id}?_method=DELETE`} method="POST">
+                          <input type="submit" value="DELETE"/>
+                      </form>
+                      <br />
               </li>
-            );
+            )
           })}
         </ul>
         <nav>
           <a href="/dortmunder/new">Add to the list</a>
         </nav>
-      </div>
+      </DefaultLayout>
     )
   }
 }
