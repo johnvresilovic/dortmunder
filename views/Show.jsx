d6d9d745fa60bc64503b8ed2bf78e49cb8ca4353
John Vresilovic <johnvresilovic@protonmail.com>
2022-12-15 10:39:40 -0500

    got edit and delete working on Show

--- views/Show.jsx
+++ views/Show.jsx
@@ -6,7 +6,11 @@ class Show extends React.Component {
         <h1>{this.props.dortmunder.title}</h1>
         <img src={this.props.dortmunder.image}></img>
         <h2>{this.props.dortmunder.price}</h2>
-        <h2>Number in stock: {this.props.dortmunder.quantity}</h2>
+        <h2>Number in stock: {this.props.dortmunder.quantity}</h2>    
+        <a href={`/dortmunder/${this.props.dortmunder._id}/edit`}>Edit</a><br />
+        <form action={`/dortmunder/${this.props.dortmunder._id}?_method=DELETE`} method="POST">
+        <input type="submit" value="DELETE"/>
+        </form>
         <nav>
           <a href="/dortmunder">See the whole series</a>
         </nav>
